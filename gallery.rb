2d337833dac340e9cc7328633b9c3f280b814adb
JT1989 <jtiao@kissmetrics.com>
2015-02-02 22:18:40 -0800

    Revised 0.4 Support multiple images based on feedback

--- gallery.rb
+++ gallery.rb
@@ -3,86 +3,88 @@ def absolute_path(file)  #takes file, "bunny-1.jpg"
   File.absolute_path(file) #gets the absolute path, starts at the root directory
 end
 
-if ARGV.size == 0
-    puts "Welcome! I can fetch the absolute path for you. What is the filename?"
-    puts "For example, type: bunny-4.jpg"
-    file_name = gets.chomp
-    puts absolute_path(file_name)
-  else
-    file_name = ARGV[0]
-    puts absolute_path(file_name)
-end
+# if ARGV.size == 0
+#     puts "Welcome! I can fetch the absolute path for you. What is the filename?"
+#     puts "For example, type: bunny-4.jpg"
+#     file_name = gets.chomp
+#     puts absolute_path(file_name)
+#   else
+#     file_name = ARGV[0]
+#     puts absolute_path(file_name)
+# end
 
 #[v0.2] Output a full image tag (<img>)
 def image_tag(file_name)
   return "<img src=\"#{file_name}\">"
 end
 
-# #[v0.3] Generate a full, valid HTML page
-def generate_HTML_page(image_tag)
+# # #[v0.3] Generate a full, valid HTML page
+# def generate_HTML_page(image_tag)
+#   full_html = <<-HTML
+#     <!DOCTYPE html>
+#     <html>
+#       <head>
+#         <title>My Gallery</title>
+#       </head>
+#       <body>
+#         <h1>My Gallery</h1>
+#       </body>
+#     </html>
+#   HTML
+
+#   return full_html
+# end
+
+# #[v0.4] Support multiple images
+def top_html
   full_html = <<-HTML
     <!DOCTYPE html>
-    <html>
-      <head>
-        <title>My Gallery</title>
-      </head>
+      <html>
+        <head>
+          <style>
+          h1 {
+            color: #000;
+            font-family: Georgia;
+            font-size: 20px;
+            line-height: 22px;
+            font-weight: 700;
+            font-style: italic;
+            letter-spacing: 0.01em;
+            word-spacing: -0.1em;
+          }
+          img {
+            width: 225px;
+            height: 225px;
+            border-color: #fff;
+            border-width: 1px;
+            box-shadow: 1px 1px 2px lightgray;
+          }
+          </style>
+          <title>My Gallery</title>
+        </head>
       <body>
         <h1>My Gallery</h1>
-      </body>
-    </html>
-  HTML
-
+    HTML
   return full_html
 end
 
-# #[v0.4] Support multiple images
-# def top_html
-# full_html = <<-HTML
-# <!DOCTYPE html>
-# <html>
-# <head>
-# <style>
-# h1 {
-#   color: #000;
-#   font-family: Georgia;
-#   font-size: 20px;
-#   line-height: 22px;
-#   font-weight: 700;
-#   font-style: italic;
-#   letter-spacing: 0.01em;
-#   word-spacing: -0.1em;
-# }
-# img {
-#   width: 225px;
-#   height: 225px;
-#   border-color: #fff;
-#   border-width: 1px;
-#   box-shadow: 1px 1px 2px lightgray;
-# }
-# </style>
-# <title>My Gallery</title>
-# </head>
-# <body>
-# <h1>My Gallery</h1>
-# HTML
-#   return full_html
-# end
+def image_tag_list(files)
+  result = []
+  files.each do |file|
+    result.push(image_tag(absolute_path(file)))
+  end
+  string = result.join("\n") #item to the end of the array
 
-# def image_tag_list
-#   ARGV.each do |file|
-#     absolute_path = absolute_path(file)
-#     image_tag_source_path = image_tag(absolute_path)
-#     puts "#{image_tag_source_path}"
-#   end
-# end
+  return string
+end
 
-# def bottom_html
-# full_html = <<-HTML
-# </body>
-# </html>
-# HTML
-#   return full_html
-# end
+def bottom_html
+  full_html = <<-HTML
+      </body>
+      </html>
+    HTML
+  return full_html
+end
 
 # #[v0.5] Make it look nicer with CSS
 
@@ -132,12 +134,13 @@ end
   # image_tag_source_path = image_tag(absolute_path)
   # puts generate_HTML_page(image_tag_source_path)
 
-#   #[v0.4]
-# if __FILE__ == $PROGRAM_NAME
-#   puts top_html
-#   image_tag_list
-#   puts bottom_html
-# end
+  #[v0.4]
+  if __FILE__ == $PROGRAM_NAME
+    puts top_html
+    puts image_tag_list(ARGV)
+    puts bottom_html
+  end
+
 
 # #[v1.0]
 # if __FILE__ == $PROGRAM_NAME
